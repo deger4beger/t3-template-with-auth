514466bb9b68e848cceeb2bab3e6790c3846b440
deger4beger <deger.begerrr@gmail.com>
2022-09-24 18:49:26 +0600

    cleanup

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -3,31 +3,16 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
-/**
- * Replace this with an object if you want to pass things to createContextInner
- */
 type CreateContextOptions = Record<string, never>;
 
-/** Use this helper for:
- * - testing, where we dont have to Mock Next.js' req/res
- * - trpc's `createSSGHelpers` where we don't have req/res
- **/
-export const createContextInner = async (opts: CreateContextOptions) => {
+export const createContext = async (
+  { req, res }: trpcNext.CreateNextContextOptions,
+) => {
   return {
     prisma,
-  };
-};
-
-/**
- * This is the actual context you'll use in your router
- * @link https://trpc.io/docs/context
- **/
-export const createContext = async (
-  opts: trpcNext.CreateNextContextOptions,
-) => {
-  return await createContextInner({
-
-  });
+    req,
+    res
+  }
 };
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
