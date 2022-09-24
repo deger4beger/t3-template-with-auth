90e110a9774b3d1db8b5c7cf3021784172b34c85
deger4beger <deger.begerrr@gmail.com>
2022-09-24 21:12:39 +0600

    cleanup

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -4,8 +4,6 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { authGuard } from "../common/auth-guard";
 import { prisma } from "../db/client";
 
-type CreateContextOptions = Record<string, never>;
-
 export const createContext = async (
   ctx: trpcNext.CreateNextContextOptions,
 ) => {
