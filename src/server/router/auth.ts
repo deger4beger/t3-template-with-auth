4c8510f5947fe34406543fd79e9f4a3694b48dea
deger4beger <deger.begerrr@gmail.com>
2022-09-24 15:35:00 +0600

    signup initial

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -1,18 +1,41 @@
 import { createRouter } from "./context";
 import { z } from "zod";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import { getSignedToken } from "../../utils/jwt";
+
+const userOutput = z.object({
+  userData: z.object({
+    id: z.string(),
+    email: z.string().email(),
+    name: z.string(),
+    role: z.enum(["STUDENT", "TEACHER"]),
+    createdAt: z.string(),
+  }),
+  jwt: z.string(),
+})
+
+export type User = z.infer<typeof userOutput>
 
 export const authRouter = createRouter()
-  .query("getPermission", {
+  .query("signup", {
     input: z
       .object({
         email: z.string().email(),
-        password: z.string()
+        password: z.string().min(8).max(20),
+        name: z.string().min(10).max(50),
+        role: z.enum(["STUDENT", "TEACHER"]),
       }),
-    resolve({ input, ctx }) {
-      return ctx.prisma.user.findUnique({
-        where: {
-          email: input.email
-        }
-      })
+    output: userOutput,
+    async resolve({ input, ctx }) {
+      const userPayload = {
+        ...input,
+        password: await bcrypt.hash(input.password, 10)
+      };
+      const { password, ...user } = await ctx.prisma.user.create({ data: userPayload });
+      return {
+        userData: user,
+        jwt: getSignedToken(user)
+      }
     },
   })
